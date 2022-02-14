274f10b9fb23e101129692f0cc44e88f003cfc56
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-02-14 14:08:26 +0300

    md files created

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
 import * as globalFunctions from './modules/functions.js';
-
 globalFunctions.isWebp();
+
+import Vue from 'vue/dist/vue.js';
+import $ from 'jquery';
+
+import Header from '../blocks/modules/header/header.js';
+
+
+window.app = new Vue({
+    el: '#app',
+    data: () => ({
+        isMounted: false,
+        header: new Header({
+            someVareible: 'someVareible'
+        })
+    }),
+    mounted() {
+        this.isMounted = true;
+        this.header.init();
+    }
+});
