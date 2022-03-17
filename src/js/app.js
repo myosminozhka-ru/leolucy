473f861fb3e9fb7c02c149d7eb33f3e5e30ba697
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-03-17 23:13:52 +0300

    window width tracking added

--- src/js/app.js
+++ src/js/app.js
@@ -10,12 +10,30 @@ window.app = new Vue({
     el: '#app',
     data: () => ({
         isMounted: false,
+        sizes: {
+            tablet: 1024,
+            mobile: 768,
+            window: window.innerWidth
+        },
         header: new Header({
             someVareible: 'someVareible'
         })
     }),
-    mounted() {
+    beforeCreate() {        
+        window.addEventListener('resize', () => {
+            this.sizes.window = window.innerWidth;
+        });
+    },
+    beforeMount() {
         this.isMounted = true;
         this.header.init();
-    }
+    },
+    computed: {
+        isMobile: function () {
+            return this.sizes.window < this.sizes.mobile;
+        },
+        isTablet: function () {
+            return this.sizes.window < this.sizes.tablet && this.sizes.window > this.sizes.mobile;
+        }
+    },
 });
