a55321053c7474256ec9e09ad83f062f950e1520
Гончаров Сергей <Goncharov-5769@main-fer.fnpz.uz>
2022-06-23 11:49:35 +0500

    Доработка

--- src/pug/modules/calculator/calculator.js
+++ src/pug/modules/calculator/calculator.js
@@ -43,22 +43,29 @@ export default class Calculator {
                     if (this.weight > 35) this.resultWeight = 480;
                 }
                 break;
-            case 'Кошке':
-                if (this.weight <= 0.3) this.resultWeight = 21;
-                if (this.weight > 0.3 && this.weight <= 0.4) this.resultWeight = 28;
-                if (this.weight > 0.4 && this.weight <= 0.5) this.resultWeight = 35;
-                if (this.weight > 0.5 && this.weight <= 0.6) this.resultWeight = 42;
-                if (this.weight > 0.6 && this.weight <= 0.7) this.resultWeight = 49;
-                if (this.weight > 0.7 && this.weight <= 0.8) this.resultWeight = 56;
-                if (this.weight > 0.8 && this.weight <= 0.9) this.resultWeight = 63;
-                if (this.weight > 0.9 && this.weight <= 1) this.resultWeight = 70;
-                if (this.weight > 1 && this.weight <= 2) this.resultWeight = 140;
-                if (this.weight > 2 && this.weight <= 3) this.resultWeight = 210;
-                if (this.weight > 3 && this.weight <= 4) this.resultWeight = 280;
-                if (this.weight > 4 && this.weight <= 5) this.resultWeight = 350;
-                if (this.weight > 5 && this.weight <= 6) this.resultWeight = 420;
-                if (this.weight > 6 && this.weight <= 7) this.resultWeight = 490;
-                if (this.weight > 7) this.resultWeight = 560;
+            case 'Кошке':    
+                if (this.age <= 0.4) {     
+                    if (this.weight <= 0.3) this.resultWeight = 25;                    
+                    if (this.weight > 0.4 && this.weight <= 0.5) this.resultWeight = 45;                    
+                    if (this.weight > 0.9 && this.weight <= 1) this.resultWeight = 70;                    
+                    if (this.weight > 1 && this.weight <= 2) this.resultWeight = 110;                    
+                    if (this.weight > 2 && this.weight <= 3) this.resultWeight = 145;                
+                } else if (this.age > 0.4 && this.age < 1) {         
+                    if (this.weight > 0.9 && this.weight <= 1) this.resultWeight = 55;                    
+                    if (this.weight > 1 && this.weight <= 2) this.resultWeight = 90;                    
+                    if (this.weight > 2 && this.weight <= 3) this.resultWeight = 120;                    
+                    if (this.weight > 3 && this.weight <= 4) this.resultWeight = 140;                    
+                    if (this.weight > 5 && this.weight <= 6) this.resultWeight = 190;                
+                } else {              
+                    if (this.weight > 1 && this.weight <= 2) this.resultWeight = 45;                   
+                    if (this.weight > 2 && this.weight <= 3) this.resultWeight = 60;                    
+                    if (this.weight > 3 && this.weight <= 4) this.resultWeight = 70;                    
+                    if (this.weight > 4 && this.weight <= 5) this.resultWeight = 80;                    
+                    if (this.weight > 5 && this.weight <= 6) this.resultWeight = 90;                    
+                    if (this.weight > 6 && this.weight <= 7) this.resultWeight = 100;                    
+                    if (this.weight > 6 && this.weight <= 8) this.resultWeight = 110;                    
+                    if (this.weight >= 9) this.resultWeight = 120;                
+                }  
                 break;
             default:
                 console.log('Default');
