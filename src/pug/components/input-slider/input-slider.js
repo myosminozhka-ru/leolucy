02b6dea68c7cd371008f3b1323d1143888496604
Sergey Goncharovр <doimpos@gmail.com>
2022-08-02 20:57:13 +0500

    Правка калькулятора

--- src/pug/components/input-slider/input-slider.js
+++ src/pug/components/input-slider/input-slider.js
@@ -5,7 +5,69 @@ export default class InputSlider {
 
     init() {
         setTimeout(() => {
-            document.querySelectorAll('.input-slider').forEach(element => {
+            document.querySelectorAll('.calculator__slider').forEach(element => {
+                const startValue = element.dataset.startValue ? +element.dataset.startValue : 0;
+                const min = element.dataset.min ? +element.dataset.min : 0;
+                const max = element.dataset.max ? +element.dataset.max : 0;
+
+                noUiSlider.create(element, {
+                    start: startValue,
+                    connect: 'lower',
+                    tooltips: true,
+                    step: 0.1,
+                    format: {
+                        to: function (value) {
+                            return Number(value).toFixed(1);
+                        },
+                        from: function (value) {
+                            return Number(value).toFixed(1);
+                        }
+                    },
+                    range: {
+                        'min': min,
+                        'max': max
+                    }
+                }).on('update', function (values) {
+                    if (this.target.dataset.type === 'weight') {
+                        app.calculator.updateWeight(values[0]);
+                    }
+                    if (this.target.dataset.type === 'age') {
+                        app.calculator.updateAge(values[0]);
+                    }
+                });
+            });
+            document.querySelectorAll('.calculator__slider2').forEach(element => {
+                const startValue = element.dataset.startValue ? +element.dataset.startValue : 0;
+                const min = element.dataset.min ? +element.dataset.min : 0;
+                const max = element.dataset.max ? +element.dataset.max : 0;
+
+                noUiSlider.create(element, {
+                    start: startValue,
+                    connect: 'lower',
+                    tooltips: true,
+                    step: 0.1,
+                    format: {
+                        to: function (value) {
+                            return Number(value).toFixed(1);
+                        },
+                        from: function (value) {
+                            return Number(value).toFixed(1);
+                        }
+                    },
+                    range: {
+                        'min': min,
+                        'max': max
+                    }
+                }).on('update', function (values) {
+                    if (this.target.dataset.type === 'weight') {
+                        app.calculator.updateWeight(values[0]);
+                    }
+                    if (this.target.dataset.type === 'age') {
+                        app.calculator.updateAge(values[0]);
+                    }
+                });
+            });
+            document.querySelectorAll('.calculator__slider3').forEach(element => {
                 const startValue = element.dataset.startValue ? +element.dataset.startValue : 0;
                 const min = element.dataset.min ? +element.dataset.min : 0;
                 const max = element.dataset.max ? +element.dataset.max : 0;
