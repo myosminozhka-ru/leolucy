cb360ff0ba44c03e064c7a94e49d13885514e284
Sergey Goncharovр <doimpos@gmail.com>
2022-09-10 01:35:06 +0500

    Правки uikit

--- src/pug/modules/philosophy-slider/philosophy-slider.js
+++ src/pug/modules/philosophy-slider/philosophy-slider.js
@@ -3,11 +3,7 @@ import { stringClassesArray } from '../../../js/utils.js';
 
 export default class PhilosophySlider {
     constructor() {
-        this.domSlider = document.querySelector('.philosophy-slider');
-        this.domSliderSwiper = this.domSlider?.querySelector('.philosophy-slider__swiper');
-        this.domPrev = this.domSlider?.querySelector('.philosophy-slider__arrow--prev');
-        this.domNext = this.domSlider?.querySelector('.philosophy-slider__arrow--next');
-        this.domPagination = this.domSlider?.querySelector('.philosophy-slider__dots');
+        this.domSlider = document.querySelectorAll('.philosophy-slider');
     }
 
     init() {
@@ -17,23 +13,29 @@ export default class PhilosophySlider {
     initSlider() {
         if (this.domSlider) {
             setTimeout(() => {
-                new Swiper(stringClassesArray(Array.from(this.domSliderSwiper.classList)), {
-                    modules: [Navigation, Pagination],
-                    speed: 1000,
-                    slidesPerView: 1,
-                    loop: true,
-                    autoHeight: true,
-                    navigation: {
-                        prevEl: stringClassesArray(Array.from(this.domPrev.classList)),
-                        nextEl: stringClassesArray(Array.from(this.domNext.classList)),
-                    },
-                    pagination: {
-                        el: stringClassesArray(Array.from(this.domPagination.classList)),
-                        bulletClass: 'philosophy-slider__dot',
-                        bulletActiveClass: 'philosophy-slider__dot--active',
-                        clickable: true
-                    }
-                });
+                this.domSlider.forEach(element => {
+                    const domSliderSwiper = element?.querySelector('.philosophy-slider__swiper');
+                    const domPrev = element?.querySelector('.philosophy-slider__arrow--prev');
+                    const domNext = element?.querySelector('.philosophy-slider__arrow--next');
+                    const domPagination = element?.querySelector('.philosophy-slider__dots');
+                    new Swiper(stringClassesArray(Array.from(domSliderSwiper.classList)), {
+                        modules: [Navigation, Pagination],
+                        speed: 1000,
+                        slidesPerView: 1,
+                        loop: true,
+                        autoHeight: true,
+                        navigation: {
+                            prevEl: stringClassesArray(Array.from(domPrev.classList)),
+                            nextEl: stringClassesArray(Array.from(domNext.classList)),
+                        },
+                        pagination: {
+                            el: stringClassesArray(Array.from(domPagination.classList)),
+                            bulletClass: 'philosophy-slider__dot',
+                            bulletActiveClass: 'philosophy-slider__dot--active',
+                            clickable: true
+                        }
+                    });
+                })  
             }, 0);
         }
     }
